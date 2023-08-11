286bcca10ecc36c5ecfa3d8effdd7910fae6916b
Gunjan-Chhabra <140606035+Gunjan-Chhabra@users.noreply.github.com>
2023-08-11 12:10:12 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "php-kuiz") or die("could not connect" . mysqli_error($conn) ) ;
+$conn = mysqli_connect("database-1.claaotx5w7yj.us-east-1.rds.amazonaws.com", "admin", "Asdfg1234*", "database-1") or die("could not connect" . mysqli_error($conn) ) ;
 ?>
